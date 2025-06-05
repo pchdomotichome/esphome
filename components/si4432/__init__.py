ae43909daef834a345f4d7540f179307dee04cc6
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 17:45:31 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -7,20 +7,21 @@ CODEOWNERS = ["@pchdomotichome"]
 DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
-SI4432Component = si4432_ns.class_("SI4432Component", cg.Component, spi.SPIDevice)
+Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SI4432Component),
-    cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,  # CS como número entero
-})
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(Si4432Component),
+        cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
+        cv.Required(CONF_CS_PIN): cv.gpio_pin,  # ← Correcto para ESPHome 2025.5.2
+    }
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
 
-    # Convertimos GPIO numérico a expresión
-    cs = cg.gpio_num(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs))
+    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    var.set_cs_pin(cs)
 
