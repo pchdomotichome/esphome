ea35072665dba7af612ac8d9bbbce96bc393c998
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 09:29:41 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,13 +1,12 @@
-from esphome.components import spi, gpio
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
+from esphome.components import spi, gpio
+from esphome.const import CONF_ID
 
-CODEOWNERS = ["@pchdomotichome"]
-DEPENDENCIES = ["spi"]
+from . import si4432_ns
 
-si4432_ns = cg.esphome_ns.namespace("si4432")
-Si4432Component = si4432_ns.class_("Si4432Component", cg.PollingComponent, spi.SPIDevice)
+Si4432Component = si4432_ns.class_("Si4432Component", cg.Component)
 
 CONF_SPI_ID = "spi_id"
 CONF_CS_PIN = "cs_pin"
@@ -20,16 +19,3 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_IRQ_PIN): gpio.input_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-
-    # Registrar como dispositivo SPI
-    spi_dev = await spi.register_spi_device(config[CONF_SPI_ID], config[CONF_CS_PIN])
-    cg.add(var.set_spi_device(spi_dev))
-
-    # Configurar IRQ
-    irq_pin = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
-    cg.add(var.set_irq_pin(irq_pin))
-
-    # Registrar componente
-    await cg.register_component(var, config)
