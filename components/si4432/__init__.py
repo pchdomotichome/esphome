69659a990ab863f636fd48f868008af4b933186e
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 12:44:49 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -12,7 +12,7 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Si4432Component),
         cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-        cv.Optional(CONF_CS_PIN): gpio.gpio_pin_schema,  # ✅ correcto uso desde el módulo gpio
+        cv.Optional(CONF_CS_PIN): cv.GPIOPin,  # ✅ CORRECTO uso actualizado
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -21,12 +21,12 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Obtener el SPI parent correctamente
+    # Registrar como dispositivo SPI
     spi_parent = await cg.get_variable(config[CONF_SPI_ID])
     await spi.register_spi_device(var, spi_parent)
 
-    # Configurar CS pin si está presente
+    # Si se define CS pin, asignarlo
     if CONF_CS_PIN in config:
-        cs = await gpio.gpio_pin_expression(config[CONF_CS_PIN])
+        cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
         cg.add(var.set_cs_pin(cs))
 
