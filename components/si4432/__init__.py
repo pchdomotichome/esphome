a3d28e9368c6201257960aff28d9f285116c736f
pchdomotichome <pchdomotichome@gmail.com>
2025-06-04 22:42:50 -0300

    updated code

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,22 +1,25 @@
-import esphome.codegen as cg
+from esphome.components import spi
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome import pins
+from esphome.core import CORE
+from esphome.cpp_generator import Pvariable
+from esphome.cpp_helpers import setup_component
+from esphome.cpp_types import App, Component
 
-DEPENDENCIES = ["spi"]
-AUTO_LOAD = ["spi"]
+DEPENDENCIES = ['spi']
 
-si4432_ns = cg.esphome_ns.namespace("si4432")
-Si4432Component = si4432_ns.class_("Si4432Component", cg.Component)
+CONF_SI4432_ID = "si4432_id"
+
+si4432_ns = esphome_ns.namespace("si4432")
+Si4432Component = si4432_ns.class_("Si4432Component", Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs_pin))
-
+def to_code(config):
+    var = Pvariable(config[CONF_ID], Si4432Component.new())
+    yield spi.setup_spi_device(var, config[CONF_CS_PIN])
+    yield setup_component(var, config)
--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
 
@@ -7,13 +8,9 @@ namespace si4432 {
 
 class Si4432Component : public Component, public spi::SPIDevice {
  public:
-  void set_cs_pin(uint8_t cs) { this->cs_pin_ = cs; }
-
   void setup() override;
   void loop() override;
 
- protected:
-  uint8_t cs_pin_;
   void write_register(uint8_t reg, uint8_t value);
   uint8_t read_register(uint8_t reg);
 };
