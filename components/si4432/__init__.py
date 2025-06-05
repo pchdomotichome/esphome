5648f2e0b5142c7b5a5ee0717e1e33bc80030a7d
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 10:57:47 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,22 +1,21 @@
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import spi, gpio
-from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
+from esphome.components import spi
+from esphome.const import CONF_ID, CONF_CS_PIN
 
-CODEOWNERS = ["@tuusuario"]
-DEPENDENCIES = ["spi"]
+from esphome import pins
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
-    cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): gpio.output_pin_schema,
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+}).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await spi.register_spi_device(var, config[CONF_SPI_ID], config[CONF_CS_PIN])
+    await spi.register_spi_device(var, config)
+    cg.add(var.set_cs_pin(await cg.gpio_pin_expression(config[CONF_CS_PIN])))
+
