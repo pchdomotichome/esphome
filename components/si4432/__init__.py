55d86d5b9f18d22505100476a37e70edb5679d31
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 08:58:36 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,27 +1,34 @@
 
-import esphome.codegen as cg
 import esphome.config_validation as cv
+import esphome.codegen as cg
 from esphome.components import spi, gpio
-from esphome.const import CONF_ID, CONF_CS_PIN, CONF_IRQ_PIN
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
-Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
+SI4432Component = si4432_ns.class_("SI4432Component", cg.Component, spi.SPIDevice)
 
+CONF_CS_PIN = "cs_pin"
 CONF_IRQ_PIN = "irq_pin"
+CONF_SPI_ID = "spi_id"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Si4432Component),
-    cv.Required(CONF_CS_PIN): cv.pin,
-    cv.Required(CONF_IRQ_PIN): cv.pin,
-}).extend(spi.spi_device_schema())
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(SI4432Component),
+            cv.Required(CONF_CS_PIN): gpio.output_pin_schema,
+            cv.Required(CONF_IRQ_PIN): gpio.input_pin_schema,
+            cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await spi.register_spi_device(var, config)
-    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs))
-    irq = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
-    cg.add(var.set_irq_pin(irq))
+    await spi.register_spi_device(var, config[CONF_SPI_ID], config[CONF_CS_PIN])
+    irq_pin = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
+    cg.add(var.set_irq_pin(irq_pin))
+
