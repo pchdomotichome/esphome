89c7258ba9364f39493d832b9dbf08dc3d7a8fc5
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 18:28:31 -0300

    Update si4432.h

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
-#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace si4432 {
@@ -12,13 +11,10 @@ class Si4432Component : public Component, public spi::SPIDevice {
   void setup() override;
   void loop() override;
 
-  void set_cs_pin(GPIOPin *cs_pin);
+  void set_cs_pin(GPIOPin *pin) { this->cs_pin_ = pin; }
 
  protected:
   GPIOPin *cs_pin_;
-
-  void write_register(uint8_t address, uint8_t value);
-  uint8_t read_register(uint8_t address);
 };
 
 }  // namespace si4432
