48c989afbe2c2dcf3b2df78033cf6d35cd3b8cf1
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 15:33:42 -0300

    1

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,21 +1,30 @@
-
-import esphome.codegen as cg
+from esphome import pins
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
 
+from esphome.cpp_generator import Pvariable
+from esphome.cpp_helpers import gpio_output_pin_expression, setup_component
+from esphome.cpp_types import App, Component
+
+CODEOWNERS = ["@pchdomotichome"]
 DEPENDENCIES = ["spi"]
 
-si4432_ns = cg.esphome_ns.namespace("si4432")
-SI4432Component = si4432_ns.class_("SI4432Component", cg.Component, spi.SPIDevice)
+CONF_SI4432_ID = "si4432_id"
+
+si4432_ns = App.namespace("si4432")
+Si4432Component = si4432_ns.class_("Si4432Component", Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SI4432Component),
-    cv.Required(CONF_CS_PIN): cv.Pin,
-}).extend(spi.spi_device_schema())
+    cv.GenerateID(): cv.declare_id(Si4432Component),
+    cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
+    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+}).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await spi.register_spi_device(var, config)
-    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+
+def to_code(config):
+    rhs = si4432_ns.Si4432Component.new()
+    si4432 = Pvariable(config[CONF_ID], rhs)
+    yield spi.register_spi_device(si4432, config)
+    yield gpio_output_pin_expression(config[CONF_CS_PIN])
+    yield setup_component(si4432, config)
--- components/si4432/si4432.cpp
+++ components/si4432/si4432.cpp
@@ -1,60 +1,19 @@
-
-#include "esphome.h"
 #include "esphome/core/log.h"
-#include "esphome/components/spi/spi.h"
+#include "si4432.h"
 
 namespace esphome {
 namespace si4432 {
 
 static const char *const TAG = "si4432";
 
-class SI4432Component : public Component, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1MHZ> {
- public:
-  void setup() override {
-    ESP_LOGI(TAG, "Setting up SI4432...");
-    this->spi_setup();
+void Si4432Component::setup() {
+  ESP_LOGI(TAG, "Setting up Si4432 receiver...");
+  // Aquí irá la inicialización real del chip Si4432
+}
 
-    uint8_t part_num = this->read_register(0x01);  // Device Type
-    ESP_LOGI(TAG, "SI4432 Device ID: 0x%02X", part_num);
-
-    // Modo receptor (simplificado)
-    this->write_register(0x07, 0x01);  // Operating Mode: Ready
-  }
-
-  void loop() override {
-    uint8_t status = this->read_register(0x03);  // Interrupt Status 1
-    if (status & 0x02) {  // Packet Received
-      ESP_LOGI(TAG, "Packet received");
-
-      uint8_t len = this->read_register(0x4B);  // Received packet length
-      std::vector<uint8_t> buffer;
-      for (int i = 0; i < len; i++) {
-        buffer.push_back(this->read_register(0x7F));  // FIFO
-      }
-
-      ESP_LOGI(TAG, "Data:");
-      for (auto byte : buffer) {
-        ESP_LOGI(TAG, "0x%02X ", byte);
-      }
-    }
-  }
-
- protected:
-  void write_register(uint8_t reg, uint8_t value) {
-    this->enable();
-    this->spi_write_byte(reg | 0x80);  // Write operation
-    this->spi_write_byte(value);
-    this->disable();
-  }
-
-  uint8_t read_register(uint8_t reg) {
-    this->enable();
-    this->spi_write_byte(reg & 0x7F);  // Read operation
-    uint8_t value = this->spi_read_byte();
-    this->disable();
-    return value;
-  }
-};
+void Si4432Component::loop() {
+  // Aquí iría la lógica para recibir datos
+}
 
 }  // namespace si4432
 }  // namespace esphome
