75a9635194a168e0b2bfe5b6cda63dfae27a50c3
pchdomotichome <pchdomotichome@gmail.com>
2025-06-04 23:45:27 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -2,6 +2,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
+from esphome import pins
 from esphome.const import CONF_ID, CONF_IRQ_PIN, CONF_CS_PIN
 
 DEPENDENCIES = ["spi"]
