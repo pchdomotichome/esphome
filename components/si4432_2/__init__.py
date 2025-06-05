f8aaa7a5b75b2bfb025ad363da45eab5f621235d
pchdomotichome <pchdomotichome@gmail.com>
2025-06-04 23:16:12 -0300

    changed cs pin to GPIO16

--- components/si4432_2/__init__.py
+++ components/si4432_2/__init__.py
@@ -1,24 +1,24 @@
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
+from esphome import pins
 from esphome.const import CONF_ID, CONF_CS_PIN
 
 DEPENDENCIES = ["spi"]
-
-CONF_IRQ_PIN = "irq_pin"
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
-    cv.Required(CONF_CS_PIN): spi.validate_spi_cs_pin,
-    cv.Required(CONF_IRQ_PIN): cv.gpio_input_pin,
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.SPI_DEVICE_SCHEMA)
+    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+}).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    cg.add(var.set_cs_pin(cs))
     await spi.register_spi_device(var, config)
-    cg.add(var.set_irq_pin(config[CONF_IRQ_PIN]))
+
+
--- components/si4432_2/si4432.h
+++ components/si4432_2/si4432.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
@@ -6,15 +5,14 @@
 namespace esphome {
 namespace si4432 {
 
-class Si4432Component : public Component, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1MHZ> {
+class Si4432Component : public Component, public spi::SPIDevice {
  public:
   void setup() override;
   void loop() override;
 
-  void set_irq_pin(GPIOPin *pin) { irq_pin_ = pin; }
-
  protected:
-  GPIOPin *irq_pin_;
+  void write_register(uint8_t reg, uint8_t value);
+  uint8_t read_register(uint8_t reg);
 };
 
 }  // namespace si4432
