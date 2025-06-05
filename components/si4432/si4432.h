c678a32903d72cccc9c7c971aacd8c95003f023b
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 09:39:33 -0300

    test

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -1,24 +1,22 @@
 #pragma once
 
-#include "esphome/components/spi/spi.h"
 #include "esphome/core/component.h"
+#include "esphome/components/spi/spi.h"
 
 namespace esphome {
 namespace si4432 {
 
-class Si4432Component : public PollingComponent, public spi::SPIDevice {
+class Si4432Component : public Component, public spi::SPIDevice {
  public:
-  void update() override;
-  void setup() override;
-  void dump_config() override;
+  void setup() override {}
+  void loop() override {}
 
-  void set_cs_pin(GPIOPin *pin);
-  void set_irq_pin(GPIOPin *pin);
+  void set_irq_pin(GPIOPin *pin) { this->irq_pin_ = pin; }
 
  protected:
-  GPIOPin *cs_pin_;
   GPIOPin *irq_pin_;
 };
 
 }  // namespace si4432
 }  // namespace esphome
+
