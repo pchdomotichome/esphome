2b27ee005eefa7327eadac64e66d44d52a4e22f7
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 09:25:46 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -12,25 +12,24 @@ Si4432Component = si4432_ns.class_("Si4432Component", cg.PollingComponent, spi.S
 CONF_CS_PIN = "cs_pin"
 CONF_IRQ_PIN = "irq_pin"
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(Si4432Component),
-            cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
-            cv.Required(CONF_IRQ_PIN): pins.gpio_input_pin_schema,
-            cv.GenerateID(spi.CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-        }
-    ).extend(cv.polling_component_schema("1s"))
-)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(Si4432Component),
+    cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIDevice),
+    cv.Required(CONF_CS_PIN): gpio.output_pin_schema,
+    cv.Required(CONF_IRQ_PIN): gpio.input_pin_schema,
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    irq_pin = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
-    spi_parent = await cg.get_variable(config[spi.CONF_SPI_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
 
-    var = cg.new_Pvariable(config[cv.CONF_ID])
-    await cg.register_component(var, config)
-    await spi.register_spi_device(var, spi_parent)
-    cg.add(var.set_cs_pin(cs_pin))
+    # Registrar como dispositivo SPI
+    spi_dev = await spi.register_spi_device(config[CONF_SPI_ID], config[CONF_CS_PIN])
+    cg.add(var.set_spi_device(spi_dev))
+
+    # Configurar IRQ
+    irq_pin = await cg.gpio_pin_expression(config[CONF_IRQ_PIN])
     cg.add(var.set_irq_pin(irq_pin))
 
+    # Registrar componente
+    await cg.register_component(var, config)
