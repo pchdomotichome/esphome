dd505881a5149cb49a79309a77a0d85da0c49b24
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 17:16:36 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -11,14 +11,19 @@ SI4432 = si4432_ns.class_("SI4432", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SI4432),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.templatable(cv.int_),  # ✅ Esto es lo que funciona
+    cv.Required(CONF_CS_PIN): cv.int_,  # ← solo validamos que sea int
 })
 
 async def to_code(config):
     spi_comp = await cg.get_variable(config[CONF_SPI_ID])
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+
+    # Creamos un dict como espera gpio_pin_expression
+    cs_pin_conf = {
+        'number': config[CONF_CS_PIN],
+    }
+    cs = await cg.gpio_pin_expression(cs_pin_conf)
     cg.add(var.set_spi(spi_comp))
     cg.add(var.set_cs_pin(cs))
 
