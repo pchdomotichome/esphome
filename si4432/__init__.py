d0ca325fbc4b78725ed94ad28c0a463c09007c2a
pchdomotichome <pchdomotichome@gmail.com>
2025-06-04 22:51:33 -0300

    fixed cs pin

--- si4432/__init__.py
+++ si4432/__init__.py
@@ -1,18 +1,23 @@
-import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome import automation
+from esphome.components.spi import SPIComponent
+from esphome.core import CORE
+from esphome.cpp_helpers import setup_component
 
 CODEOWNERS = ["@pchdomotichome"]
 DEPENDENCIES = ["spi"]
+
+CONF_SI4432_ID = "si4432_id"
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
-    cv.Required(CONF_CS_PIN): spi.validate_spi_cs_pin,
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.SPI_DEVICE_SCHEMA)
+    cv.Required(CONF_CS_PIN): cv.spi_cs_pin,
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
--- si4432/si4432.cpp
+++ si4432/si4432.cpp
@@ -7,27 +7,25 @@ namespace si4432 {
 static const char *const TAG = "si4432";
 
 void Si4432Component::setup() {
-  ESP_LOGI(TAG, "Setting up Si4432...");
+  ESP_LOGI(TAG, "Setting up SI4432...");
   this->spi_setup();
 }
 
 void Si4432Component::loop() {
-  // Example read register loop
-  uint8_t reg_val = this->read_register(0x01);
-  ESP_LOGD(TAG, "Reg 0x01: 0x%02X", reg_val);
+  // Placeholder loop
 }
 
 void Si4432Component::write_register(uint8_t reg, uint8_t value) {
   this->enable();
-  this->write_byte(reg | 0x80);
-  this->write_byte(value);
+  this->transfer(reg | 0x80);
+  this->transfer(value);
   this->disable();
 }
 
 uint8_t Si4432Component::read_register(uint8_t reg) {
   this->enable();
-  this->write_byte(reg & 0x7F);
-  uint8_t value = this->read_byte();
+  this->transfer(reg & 0x7F);
+  uint8_t value = this->transfer(0);
   this->disable();
   return value;
 }
