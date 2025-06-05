77101776e18a776a251a92d19fff687f3f3214f5
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 20:09:39 -0300

    Update si4432.h

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -1,20 +1,13 @@
 #pragma once
-
 #include "esphome/core/component.h"
-#include "esphome/components/spi/spi.h"
 
 namespace esphome {
 namespace si4432 {
 
-class Si4432Component : public Component, public spi::SPIDevice {
+class Si4432Component : public Component {
  public:
   void setup() override;
   void loop() override;
-
-  void set_cs_pin(GPIOPin *pin) { this->cs_pin_ = pin; }
-
- protected:
-  GPIOPin *cs_pin_;
 };
 
 }  // namespace si4432
