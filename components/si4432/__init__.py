c0bcd73e6720d24e07954ee631bd57a6da032ac9
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 17:19:17 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -11,7 +11,7 @@ SI4432 = si4432_ns.class_("SI4432", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SI4432),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,  # ← solo validamos que sea int
+    cv.Required(CONF_CS_PIN): cv.int_,  # Aceptamos un número de pin directamente
 })
 
 async def to_code(config):
@@ -19,11 +19,9 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Creamos un dict como espera gpio_pin_expression
-    cs_pin_conf = {
-        'number': config[CONF_CS_PIN],
-    }
-    cs = await cg.gpio_pin_expression(cs_pin_conf)
+    # ✅ Este helper funciona con enteros simples
+    cs = cg.digital_pin_to_gpio_expression(config[CONF_CS_PIN])
+
     cg.add(var.set_spi(spi_comp))
     cg.add(var.set_cs_pin(cs))
 
