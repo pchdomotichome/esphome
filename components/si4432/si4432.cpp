9106786f5f76fa75bd7749cad5e2b1d4351402ca
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 20:10:05 -0300

    Update si4432.cpp

--- components/si4432/si4432.cpp
+++ components/si4432/si4432.cpp
@@ -7,11 +7,11 @@ namespace si4432 {
 static const char *const TAG = "si4432";
 
 void Si4432Component::setup() {
-  ESP_LOGI(TAG, "Si4432 setup: CS pin configured.");
+  ESP_LOGI(TAG, "Si4432 setup completo");
 }
 
 void Si4432Component::loop() {
-  ESP_LOGD(TAG, "Si4432 loop running...");
+  ESP_LOGD(TAG, "Si4432 loop ejecutándose...");
 }
 
 }  // namespace si4432
