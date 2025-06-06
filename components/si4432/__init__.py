a2ba35990bc1072b5e3c6e12b4fa6cf26300b4e3
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 14:33:59 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -3,22 +3,22 @@ import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
 
-CODEOWNERS = ["@pch"]
+CODEOWNERS = ["@pchom"]
 DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Si4432Component),
-    cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,
-})
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(Si4432Component),
+        cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
+        cv.Required(CONF_CS_PIN): cv.pin,
+    }
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    var.set_cs_pin(config[CONF_CS_PIN])  # ✅ usamos int directamente
-    await spi.register_spi_device(var, config[CONF_SPI_ID])
+    await spi.register_spi_device(var, config)  # ✅ CORRECTO
 
