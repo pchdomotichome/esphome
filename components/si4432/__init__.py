1104122f2ccb62bb1a5dc5e49341f37cbbd0df99
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 15:22:05 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -18,7 +18,7 @@ CONFIG_SCHEMA = cv.Schema(
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+#    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)  # ✅ CORRECTO para ESPHome 2025.5.x
 
