2a385a76df2d8f634d4df692eaad7a7cbff4c35c
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 21:36:45 -0300

    Update si4432.h

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/spi/spi.h"
+#include "esphome/components/spi/spi_device.h"
 
 namespace esphome {
 namespace si4432 {
 
-class Si4432Component : public Component, public spi::SPIDevice {
+class Si4432Component : public Component, public spi::SPIDevice<Si4432Component> {
  public:
   void setup() override;
   void loop() override;
