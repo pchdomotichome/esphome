9243b8c537e49b656a5c21d4ac7f8517ede245bc
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 21:19:46 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,12 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import spi
+from esphome.components import spi  # <- necesario para el validador correcto
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@tu_usuario"]
 DEPENDENCIES = ["spi"]
 
-CONF_SI4432_ID = "si4432_id"
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
@@ -14,7 +13,7 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Si4432Component),
         cv.Required("spi_id"): cv.use_id(spi.SPIComponent),
-        cv.Required("cs_pin"): cv.spi_device_cs_pin,  # ✅ Válido en ESPHome 2025.5.2
+        cv.Required("cs_pin"): spi.spi_device_cs_pin,  # ✅ CORRECTO: desde `spi`
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
