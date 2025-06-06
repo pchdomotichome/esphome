b10da229913a500ed29560bda8970d5216b774d3
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 09:49:21 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,25 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import spi, gpio
+from esphome.components import spi
 from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
 
 CODEOWNERS = ["@tucmundo"]
 DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
-Si4432Component = si4432_ns.class_("Si4432Component", cg.Component)
+Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): gpio.gpio_output_pin_schema,  # ← Esto permite usar GPIO16
+    cv.Required(CONF_CS_PIN): cv.int_,  # ✅ ← Pin como entero simple
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
     parent = await cg.get_variable(config[CONF_SPI_ID])
-    await spi.register_spi_device(var, parent, cs)
+    await spi.register_spi_device(var, parent, config[CONF_CS_PIN])  # ✅ ← no usar gpio_pin_expression
 
