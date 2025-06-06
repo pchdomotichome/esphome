225644d2247f8ed0009ab6d9aba33bab6edaf7aa
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 13:04:50 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_SPI_ID, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
 
-CODEOWNERS = ["@tucmundo"]
+CODEOWNERS = ["@pch"]
+
 DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
@@ -12,16 +13,16 @@ Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevic
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,
+    cv.Required(CONF_CS_PIN): cv.int_,  # Recibe directamente el número de pin (ej: 5 o 16)
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # SPI parent resolution: get the actual SPIComponent object
-    spi_parent = await cg.get_variable(config[CONF_SPI_ID])
-    await spi.register_spi_device(var, spi_parent)
+    # Configuración del CS pin directamente
+    var.set_cs_pin(config[CONF_CS_PIN])
 
-    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+    # Registrar el dispositivo SPI usando directamente el ID
+    await spi.register_spi_device(var, config[CONF_SPI_ID])
 
