ff7bec5b72d98ae3110a271c0b106c6038688209
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 18:06:39 -0300

    Update si4432.cpp

--- components/si4432/si4432.cpp
+++ components/si4432/si4432.cpp
@@ -23,15 +23,16 @@ void Si4432Component::loop() {
 }
 
 uint8_t Si4432Component::read_register(uint8_t reg) {
-  this->cs_->digital_write(false);         // CS LOW
-  this->transfer(reg & 0x7F);              // Enviar dirección (MSB=0 → read)
-  uint8_t value = this->transfer(0x00);    // Dummy para recibir dato
-  this->cs_->digital_write(true);          // CS HIGH
+  this->cs_->digital_write(false);               // CS LOW
+  spi::transfer(this, reg & 0x7F);               // Dirección de lectura
+  uint8_t value = spi::transfer(this, 0x00);     // Dummy para recibir dato
+  this->cs_->digital_write(true);                // CS HIGH
   return value;
 }
 
 }  // namespace si4432
 }  // namespace esphome
+
 
 
 
