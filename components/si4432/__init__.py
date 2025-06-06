388022823ca87be2f438b9dd2708e6e2e2eaa6e7
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 15:35:10 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,7 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
+from esphome.const import CONF_ID
+#from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
 
 CODEOWNERS = ["@pchom"]
 DEPENDENCIES = ["spi"]
@@ -9,12 +10,14 @@ DEPENDENCIES = ["spi"]
 si4432_ns = cg.esphome_ns.namespace("si4432")
 Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(Si4432Component),
-        cv.GenerateID(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-        cv.Required(CONF_CS_PIN): cv.int_,  # ✅ <--- compatible con spi.register_spi_device
-    }
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Si4432Component),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(spi.spi_device_schema(cs_pin_required=True))
 )
 
 async def to_code(config):
