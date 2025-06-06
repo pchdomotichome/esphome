6d89a8e4a0855027c2aeca39513e4198563b2743
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 08:37:12 -0300

    fixed transfer_byte error

--- components/si4432/si4432.cpp
+++ components/si4432/si4432.cpp
@@ -1,5 +1,6 @@
 #include "si4432.h"
 #include "esphome/core/log.h"
+#include "esphome/components/spi/spi.h"
 
 namespace esphome {
 namespace si4432 {
@@ -19,10 +20,10 @@ void Si4432Component::loop() {
   }
   this->last_read_time_ = now;
 
-  uint8_t status = this->read_register(0x07);  // Operating Mode & Function Control 1
-  uint8_t device_type = this->read_register(0x00);  // Device Type
-  uint8_t version = this->read_register(0x01);      // Version
-  uint8_t status2 = this->read_register(0x02);      // Device Status
+  uint8_t status = this->read_register(0x07);      // Operating Mode
+  uint8_t device_type = this->read_register(0x00); // Device Type
+  uint8_t version = this->read_register(0x01);     // Version
+  uint8_t status2 = this->read_register(0x02);     // Status
 
   ESP_LOGI(TAG, "SI4432 Registers:");
   ESP_LOGI(TAG, "  Reg 0x00 (Device Type)   = 0x%02X", device_type);
@@ -35,8 +36,8 @@ uint8_t Si4432Component::read_register(uint8_t reg) {
   this->cs_pin_->digital_write(false);  // CS LOW
   delayMicroseconds(1);
 
-  this->spi_->transfer_byte(reg & 0x7F);  // Clear MSB for read
-  uint8_t value = this->spi_->transfer_byte(0x00);  // Dummy byte to receive data
+  spi::transfer_byte(this->spi_, reg & 0x7F);  // Clear MSB for read
+  uint8_t value = spi::transfer_byte(this->spi_, 0x00);  // Dummy byte to receive data
 
   this->cs_pin_->digital_write(true);  // CS HIGH
   delayMicroseconds(1);
