bbe2f7fbeadfdc5500837759d0711fda14b5d5ae
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 12:26:14 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -12,19 +12,17 @@ Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevic
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,  # ← GPIO número entero
+    cv.Required(CONF_CS_PIN): cv.int_,
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Asigna CS al componente
     cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
 
-    # Registra el dispositivo SPI con 2 args (var, SPI bus)
-    await spi.register_spi_device(var, config[CONF_SPI_ID])
-
+    parent = await cg.get_variable(config[CONF_SPI_ID])
+    await spi.register_spi_device(var, parent)
 
 
 
