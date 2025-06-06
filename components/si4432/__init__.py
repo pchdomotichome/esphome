33b38bdab3b33439e7623c83f520930be2b60b55
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 12:18:24 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
+from esphome.const import CONF_ID, CONF_SPI_ID, CONF_CS_PIN
 
 CODEOWNERS = ["@tucmundo"]
 DEPENDENCIES = ["spi"]
@@ -12,15 +12,19 @@ Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevic
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.int_,  # solo número, como 16
+    cv.Required(CONF_CS_PIN): cv.int_,  # ← GPIO número entero
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # ✅ usar directamente el entero como CS pin
-    await spi.register_spi_device(var, config[CONF_SPI_ID], config[CONF_CS_PIN])
+    # Asigna CS al componente
+    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+
+    # Registra el dispositivo SPI con 2 args (var, SPI bus)
+    await spi.register_spi_device(var, config[CONF_SPI_ID])
+
 
 
 
