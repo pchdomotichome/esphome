636371c73177c8bbdd18c62ae00e2db745a57991
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 09:05:56 -0300

    fix

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -1,26 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_SPI_ID, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_SPI_ID
+
+CODEOWNERS = ["@tucmundo"]
+DEPENDENCIES = ["spi"]
 
 si4432_ns = cg.esphome_ns.namespace("si4432")
-Si4432Component = si4432_ns.class_("Si4432Component", cg.Component)
+Si4432Component = si4432_ns.class_("Si4432Component", cg.Component, spi.SPIDevice)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.Required(CONF_ID): cv.declare_id(Si4432Component),
+        cv.GenerateID(): cv.declare_id(Si4432Component),
         cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-        cv.Required(CONF_CS_PIN): cv.int_,  # ← Evitamos gpio/gpio_pin/gpio_output_pin_schema
+        cv.Required(CONF_CS_PIN): cv.pin,
     }
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    spi_dev = await cg.get_variable(config[CONF_SPI_ID])
-    cg.add(var.set_spi(spi_dev))
-
-    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
-
+    await spi.register_spi_device(var, config)
 
--- components/si4432/si4432.cpp
+++ components/si4432/si4432.cpp
@@ -1,7 +1,5 @@
 #include "si4432.h"
 #include "esphome/core/log.h"
-#include "esphome/core/helpers.h"
-#include "driver/gpio.h"
 
 namespace esphome {
 namespace si4432 {
@@ -9,41 +7,33 @@ namespace si4432 {
 static const char *const TAG = "si4432";
 
 void Si4432Component::setup() {
-  ESP_LOGI(TAG, "Si4432 setup starting");
-
-  // Configurar CS como salida y ponerlo en HIGH
-  gpio_pad_select_gpio((gpio_num_t) this->cs_pin_);
-  gpio_set_direction((gpio_num_t) this->cs_pin_, GPIO_MODE_OUTPUT);
-  gpio_set_level((gpio_num_t) this->cs_pin_, 1);
-
-  delay(10);
-  ESP_LOGI(TAG, "Si4432 setup done");
+  ESP_LOGI(TAG, "Si4432 setup: calling spi_setup()");
+  this->spi_setup();  // ← ¡Importante!
 }
 
 void Si4432Component::loop() {
   static uint32_t last_time = 0;
-  if (millis() - last_time > 5000) {
-    last_time = millis();
+  if (millis() - last_time < 5000) return;
+  last_time = millis();
 
-    uint8_t reg07 = read_register(0x07);  // Operating & Function Control 1
-    uint8_t reg0C = read_register(0x0C);  // Device Type
-    uint8_t reg0D = read_register(0x0D);  // Device Version
-    uint8_t reg02 = read_register(0x02);  // Interrupt Status 1
+  uint8_t reg07 = read_register(0x07);  // Operating & Function Control 1
+  uint8_t reg0C = read_register(0x0C);  // Device Type
+  uint8_t reg0D = read_register(0x0D);  // Device Version
+  uint8_t reg02 = read_register(0x02);  // Interrupt Status 1
 
-    ESP_LOGI(TAG, "Reg 0x07 = 0x%02X | 0x0C = 0x%02X | 0x0D = 0x%02X | 0x02 = 0x%02X",
-             reg07, reg0C, reg0D, reg02);
-  }
+  ESP_LOGI(TAG, "Reg 0x07 = 0x%02X | 0x0C = 0x%02X | 0x0D = 0x%02X | 0x02 = 0x%02X",
+           reg07, reg0C, reg0D, reg02);
 }
 
 uint8_t Si4432Component::read_register(uint8_t reg) {
-  gpio_set_level((gpio_num_t) this->cs_pin_, 0);  // CS LOW
+  this->enable();  // CS LOW
   delayMicroseconds(1);
 
-  this->spi_->transfer_byte(reg & 0x7F);  // MSB=0 → Read
-  uint8_t value = this->spi_->transfer_byte(0x00);  // Dummy write to receive
+  this->transfer(reg & 0x7F);  // Clear MSB for read
+  uint8_t val = this->transfer(0x00);  // Dummy write to receive
 
-  gpio_set_level((gpio_num_t) this->cs_pin_, 1);  // CS HIGH
-  return value;
+  this->disable();  // CS HIGH
+  return val;
 }
 
 }  // namespace si4432
