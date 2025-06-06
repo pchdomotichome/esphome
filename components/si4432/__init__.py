e2bc1f18eb687c1dfebb39301566ddbd8657ae55
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 09:31:34 -0300

    Update __init__.py

--- components/si4432/__init__.py
+++ components/si4432/__init__.py
@@ -12,14 +12,13 @@ Si4432Component = si4432_ns.class_("Si4432Component", cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Si4432Component),
     cv.Required(CONF_SPI_ID): cv.use_id(spi.SPIComponent),
-    cv.Required(CONF_CS_PIN): cv.uint8_t,  # ← número entero (GPIO) como ya funcionaba
+    cv.Required(CONF_CS_PIN): cv.uint8_t,  # como número GPIO (ej. 16)
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    parent = await cg.get_variable(config[CONF_SPI_ID])
-    cs = config[CONF_CS_PIN]  # ← directamente como número
-    await spi.register_spi_device(var, parent, cs)  # ← así sí funciona
+    # Registrar dispositivo SPI usando config como diccionario completo
+    await spi.register_spi_device(var, config)
 
