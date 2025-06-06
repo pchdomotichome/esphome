f702e10ffaaf746484ed81e264eaf927697d1412
pchdomotichome <pchdomotichome@gmail.com>
2025-06-05 22:36:51 -0300

    Update si4432.h

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
-#include "esphome/components/spi/spi_device.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
@@ -12,16 +11,13 @@ class Si4432Component : public Component, public spi::SPIDevice<spi::BIT_ORDER_M
  public:
   void setup() override;
   void loop() override;
-  float get_setup_priority() const override { return setup_priority::DATA; }
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
-  void set_status_sensor(binary_sensor::BinarySensor *sensor) { this->status_sensor_ = sensor; }
+  void set_status_sensor(binary_sensor::BinarySensor *sensor) { this->chip_ok_ = sensor; }
 
  protected:
   uint8_t read_register(uint8_t reg);
-
-  uint32_t last_read_time_{0};
-  bool chip_ok_{false};
-  binary_sensor::BinarySensor *status_sensor_{nullptr};
+  binary_sensor::BinarySensor *chip_ok_{nullptr};
 };
 
 }  // namespace si4432
