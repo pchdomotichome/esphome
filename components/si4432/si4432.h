6cd63341788c0598328f75a3489d774eaa699323
pchdomotichome <pchdomotichome@gmail.com>
2025-06-06 22:05:16 -0300

    Update si4432.h

--- components/si4432/si4432.h
+++ components/si4432/si4432.h
@@ -6,7 +6,12 @@
 namespace esphome {
 namespace si4432 {
 
-class Si4432Component : public Component {
+// Clase principal del componente
+class Si4432Component : public Component,
+                        public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST,
+                                              spi::CLOCK_POLARITY_LOW,
+                                              spi::CLOCK_PHASE_LEADING,
+                                              spi::DATA_RATE_1MHZ> {
  public:
   void setup() override;
   void loop() override;
@@ -14,11 +19,8 @@ class Si4432Component : public Component {
  protected:
   uint8_t read_register(uint8_t reg);
   void write_register(uint8_t reg, uint8_t value);
-
-  uint32_t last_log_time_{0};
 };
 
 }  // namespace si4432
 }  // namespace esphome
-
 
